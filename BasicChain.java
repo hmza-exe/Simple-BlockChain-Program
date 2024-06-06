d7a6ede558cf00ad01c03d95690c651b80a8e030
Hamza <hamzasajid516@gmail.com>
2024-06-06 06:00:11 -0700

    Re-implement BasicChain class to store blocks in ArrayList and format with GSON

--- BasicChain.java
+++ BasicChain.java
@@ -1,34 +1,32 @@
+import java.util.ArrayList;
+import com.google.gson.GsonBuilder;
+
 /**
  * The BasicChain class is used to test the blockchain implementation.
  * It creates a simple blockchain with three blocks and prints their hash values.
  */
 public class BasicChain {
+
+    // An arraylisyt to store the blockchain. Every element is a block
+    public static ArrayList<Block> blockchain = new ArrayList<Block>();
     
     /**
      * The main method is the entry point of the application.
      * It creates three blocks, each linked to the previous one,
-     * and prints the hash value of each block.
-     *
-     * @param args Command line arguments (not used).
+     * adds them to the blockchain, and prints the blockchain in JSON format.
      */
     public static void main(String[] args) {
         
-        // Creates the genesis block (The first block in the blockchain)
-        /**
-         * Important Note: Since no other block exists before the genesis block,
-         * the previous hash value is set to '0'. Subsequent blocks have their 
-         * respective previous hashes correctly allocated.
-         */
-        Block genesisBlock = new Block("The first block in the blockchain!", "0");
-        System.out.println("Hash value for the first block: " + genesisBlock.hash); 
+        // Add the genesis block to the blockchain.
+        blockchain.add(new Block("This is the genesis block!", "0"));
+        // Add the second block to the blockchain.
+        blockchain.add(new Block("This is the second block!", blockchain.get(blockchain.size() - 1).hash));
+        // Add the third block to the blockchain.
+        blockchain.add(new Block("This is the third block!", blockchain.get(blockchain.size() - 1).hash));
 
-        // Creates the second block, linked to the genesis block
-        Block secondBlock = new Block("The second block in the blockchain!", genesisBlock.hash);
-        System.out.println("Hash value for the second block: " + secondBlock.hash); 
-
-        // Creates the third block, linked to the second block.
-        Block thirdBlock = new Block("The first block in the blockchain!", secondBlock.hash);
-        System.out.println("Hash value for the third block: " + thirdBlock.hash); 
+        // Convert the blockchain into Pretty-printed JSON format and print it.
+        String blockchainJSON = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        System.out.println(blockchainJSON);
 
     }
 
